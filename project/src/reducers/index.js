2993d1d637657c6f512c75095a6e8f7ae56699dc
William Ryan <billy.ryan.rom@gmail.com>
2020-02-20 19:23:24 -0500

    Add initialState and Reducer to index.js/reducers

---
+++ project/src/reducers/index.js
@@ -0,0 +1,13 @@
+const initialState = {
+    players: [],
+    isFetching: false,
+    error: ""
+}
+
+const playerReducer = (state = initialState, action) => {
+    switch(action.type){
+        default: return state
+    }
+}
+
+export default playerReducer;
